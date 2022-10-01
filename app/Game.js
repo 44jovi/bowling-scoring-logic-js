2207c334e9d22ae53b31be7e3237d6ab68d1ec22
Jovi <kwong.jovi@gmail.com>
2022-10-01 16:19:17 +0100

    feat(Game) - pass tests for updating a frame's score within scorecard instance

--- app/Game.js
+++ app/Game.js
@@ -2,8 +2,9 @@ const Scorecard = require('./Scorecard');
 
 class Game {
   constructor(scorecard) {
-    this.scorecard = new Scorecard;
+    this.scorecard = scorecard;
     this.currentFrame = null;
+    // referring to current frame:
     this.frameScore = null;
     this.frameThrow1 = null;
     this.frameThrow2 = null;
@@ -15,7 +16,7 @@ class Game {
     this.currentFrame = frame;
   }
   
-  // how could we dynamically change the frame number?
+  // should this immediately update the frame instance?
   throw1(pins) {
     this.frameThrow1 = pins;
   }
@@ -29,11 +30,33 @@ class Game {
   }
 
   // without frame 10 scoring logic
+  // might not need the if statement block
+  // if inserting the throw1 and throw2 scores to frame instance
   setThrowsTotalScore() {
     this.frameScore = this.frameThrow1 + this.frameThrow2;
+
+    if (this.currentFrame === 1) {
+      this.scorecard.frame1.score = this.frameScore;
+    } else if (this.currentFrame === 2) {
+      this.scorecard.frame2.score = this.frameScore;
+    } else if (this.currentFrame === 3) {
+      this.scorecard.frame3.score = this.frameScore;
+    } else if (this.currentFrame === 4) {
+      this.scorecard.frame4.score = this.frameScore;
+    } else if (this.currentFrame === 5) {
+      this.scorecard.frame5.score = this.frameScore;
+    } else if (this.currentFrame === 6) {
+      this.scorecard.frame6.score = this.frameScore;
+    } else if (this.currentFrame === 7) {
+      this.scorecard.frame7.score = this.frameScore;
+    } else if (this.currentFrame === 8) {
+      this.scorecard.frame8.score = this.frameScore;
+    } else if (this.currentFrame === 9) {
+      this.scorecard.frame9.score = this.frameScore;
+    }
   }
 
-  // for use when bonus points awarded
+  // for use when bonus points awarded?
   updateFrameScore(points) {
     this.frameScore = points;
   }
--- test/Game.test.js
+++ test/Game.test.js
@@ -1,15 +1,14 @@
 const Game = require('../app/Game');
 const Scorecard = require('../app/Scorecard');
 
-
 beforeEach( () => {
-  game = new Game();
   scorecard = new Scorecard();
+  game = new Game(scorecard);
 });
 
 describe('Game class', () => {
-  describe('1 frame - frame score and bonus type update after ball throw(s)', () => {
-    it('1 frame - 2 throws, no spare, no strike', () => {      
+  describe('1 frame - frame score and bonus type on scorecard', () => {
+    it('1 frame - 2 throws, no spare', () => {      
       game.setCurrentFrame(1);
       game.throw1(1);
       game.throw2(1);
@@ -22,41 +21,19 @@ describe('Game class', () => {
       expect(game.frameThrow2).toEqual(1);
       expect(game.frameThrow3).toEqual(null);
       expect(game.frameBonusType).toEqual('none');
+
+      expect(scorecard.frame1.score).toEqual(2);
     });
 
-    it('1 frame - 2 throws, spare', () => {      
-      game.setCurrentFrame(1);
-      game.throw1(5);
-      game.throw2(5);
-      game.setThrowsTotalScore();
-      game.setFrameBonusType();
+    // it('1 frame - 2 throws, spare', () => {
 
-      expect(game.currentFrame).toEqual(1);
-      expect(game.frameScore).toEqual(10);
-      expect(game.frameThrow1).toEqual(5);
-      expect(game.frameThrow2).toEqual(5);
-      expect(game.frameThrow3).toEqual(null);
-      expect(game.frameBonusType).toEqual('spare');
-    });
+    // });
 
-    it('1 frame - 1 throw, strike', () => {      
-      game.setCurrentFrame(1);
-      game.throw1(10);
-      game.setThrowsTotalScore();
-      game.setFrameBonusType();
+    // it('1 frame - 1 throw, strike', () => {
 
-      expect(game.currentFrame).toEqual(1);
-      expect(game.frameScore).toEqual(10);
-      expect(game.frameThrow1).toEqual(10);
-      expect(game.frameThrow2).toEqual(null);
-      expect(game.frameThrow3).toEqual(null);
-      expect(game.frameBonusType).toEqual('strike');
-    });
+    // });
   });
-
   
-
-
   // 2 frames - frame 1 spare, adds frame 2's throw1 to frame 1 score
   // 2 frames - frame 1 strike, adds frame 2's throw1 and throw2 to frame 1 score
   // mock previous frame bonus type?
